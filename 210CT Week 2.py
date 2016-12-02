02cb141420b9e213d96b28dceeba817434c1675b
DamianLC <DamianLCH@hotmail.co.uk>
2016-12-02 19:01:04 +0000

    Update 210CT Week 2.py

--- 210CT Week 2.py
+++ 210CT Week 2.py
@@ -17,6 +17,87 @@ print("\n-----------\n")
 
 
 
+Task 3 - Pseudocode
+
+MATRIXADD(A,B)
+	
+	result <- list
+	rowA <- length(A)
+	colA <- length(A[0])
+	rowB <- length(B)
+	colB <- length(B[0])
+
+	if rowA ≠ rowB
+		RETURN false
+	
+	if colA ≠ colB
+		RETURN false
+
+	for i  to range[rowA]
+		row <- empty list
+		for j to range[colA]
+			row <- append A[i][j] + B[i][j]
+	result <- append(row)
+	RETURN result
+
+MATRIXSUB(A,B)
+	
+	result <- list
+	rowA <- length(A)
+	colA <- length(A[0])
+
+	if rowA ≠ rowB
+		RETURN false
+	
+	if colA ≠ colB
+		RETURN false
+
+
+	for i  to range[rowA]
+		row <- empty list
+		for j to range[colA]
+			row <- append A[i][j] - B[i][j]
+	result <- append(row)
+	RETURN result
+
+MATRIXMULT(A,B)
+
+	 
+rowA <- length(A)
+	colA <- length(A[0])
+rowB <- length(B)
+	colB <- length(B[0])
+
+	if colA ≠ rowB
+		RETURN false
+
+	result <- list
+
+	for i  to range[rowA]
+		for j to range[cola]
+			for k to range[rowB]
+				result[i][j] += a[i][k] * b[k][j]
+	RETURN result
+
+MATRIXFACTORMULT(A,B)
+
+	result <- list
+	
+	rowA <- length(A)
+	colA <- length(A[0])
+
+	for i to range(rowA)
+		row <- list
+		for j to range(cola)
+			row <- append A[i][j] * b
+		result <-append(row)
+	RETRUN result
+
+A = B*C – 2*(B+C)
+A1 <- MATRIXMULT(B,C)
+A2 <- MATRIXADD(B,C)
+A3 <- MATRIXFACTORMULT(A2,2)
+A <- A1 – A3
 
 
 
